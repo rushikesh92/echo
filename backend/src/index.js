2171e1cb5b56f5d319b3093272e934356a0d5d4f
Rushikesh Kale <rushikeshkale18k@gmail.com>
2025-11-26 11:48:50 +0530

    feat: add MongoDB connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+import authRouter from './routes/auth.route.js';
+import { connectDb } from './lib/db.js';
 
 const app = express()
+
+dotenv.config()
+const port = process.env.PORT;
 
 app.get('/' , (req,res)=>{
     res.send('hello there');
 })
 
-app.listen(8000 , ()=>{
-    console.log("App is listening to port 8000");
+
+app.listen(port , ()=>{
+    console.log(`Server is listening to port ${port}`);
+    connectDb();
 });
